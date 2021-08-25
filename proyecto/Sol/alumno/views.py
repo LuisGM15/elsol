f27abdb7b220e49ab3dd5caedc40018ecccc8e85
LuisGM15 <luisgm960@gmail.com>
2021-08-24 23:42:41 -0500

    sisis

--- proyecto/Sol/alumno/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def tareas (request):
-    return render (request, 'alumno/tareas.html')
-
-def foro(request):
-    return render (request, 'alumno/forodudas.html')
